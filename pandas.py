9052a025602f1fa68deb5c13dd345f97e282ae3f
Aakash N S <aakashns.sky@gmail.com>
2021-06-13 22:03:46 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -4,3 +4,11 @@ import pandas as pd
 url = 'https://example.com'
 pd.read_csv(url)
 
+# How to create a pivot table
+import seaborn as sns
+flights_raw_df = sns.load_dataset("flights")
+# Decide the x axis, y axis and value
+flights_df = flights_raw_df.pivot("year", "month", "passengers")
+
+
+# 
